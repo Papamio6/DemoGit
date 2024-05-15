443db18521656611d6343a58fc40d2f61b64e84c
BuildTools <unconfigured@null.spigotmc.org>
2024-05-15 22:09:49 +0200

    Meilleur code à envoyer à M

---
+++ Module3.py
@@ -0,0 +1,7 @@
+def message_secret():
+    message_code = [77, 97, 114, 116, 105, 110, 32, 116, 117, 32, 101, 115, 32, 117, 110, 32, 112, 101, 116, 105, 116,
+                    32, 110, 111,
+                    111, 98]
+    message_decode = ''
+    for x in message_code:
+        message_decode += chr(x)
